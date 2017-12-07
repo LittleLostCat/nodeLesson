d14b36dadd669009c9533f079416d1186d475caf
lostLittleCat <wanglingke7733@163.com>
2017-12-07 17:58:16 +0800

    node start

---
+++ fs.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+
+//写入文件
+//arguments:路径，写入数据，回调函数
+fs.writeFile('./data.txt','hello world',function(err){
+	if(err){
+		console.log('写入文件错误')
+	}else{
+		console.log('写入文件成功')
+	}
+})
+//读取文件
+//arguments:路径，回调函数
+fs.readFile('./data.txt',function(err,data){
+	if(err){
+		throw err
+	}else{
+		console.log(data)
+	}
+})
