ccd8d0a109e105d76818220fc6e0fd391f64d62b
littleLostCat <wanglingke7733@163.com>
2018-05-13 16:08:41 +0800

    readsync

--- fs.js
+++ fs.js
@@ -2,16 +2,16 @@ var fs = require('fs')
 
 //写入文件
 //arguments:路径，写入数据，回调函数
-fs.writeFile('./data.txt','hello world',function(err){
-	if(err){
-		console.log('写入文件错误')
-	}else{
-		console.log('写入文件成功')
-	}
-})
+// fs.writeFile('./data.txt','hello world',function(err){
+// 	if(err){
+// 		console.log('写入文件错误')
+// 	}else{
+// 		console.log('写入文件成功')
+// 	}
+// })
 //读取文件
 //arguments:路径，回调函数
-fs.readFile('./data.txt',function(err,data){
+fs.readFile('./data.txt', 'utf-8',function(err,data){
 	if(err){
 		throw err
 	}else{
